99852810f9fda8dda6bafb43b02831d6d16826f6
Felix Godfree <felixfgodfree@gmail.com>
2018-09-03 15:21:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,31 @@
-//define functions here
+function getIt() {
+  $('p').on('click'
+  , function(){
+    alert("Hey!");
+    return;
+  });
+  return;
+}
+
+function frameIt() {
+  $('img').on('load'
+  , function(){
+    $('img').class = 'tasty';
+    return;
+  });
+  return;
+}
+
+function pressIt() {
+  $('form input:first-child').on('keydown'
+  , function(){
+    if ($('form input:first-child').val === "G" || $('form input:first-child').val === "g") {
+      alert('You pressed G!');
+      return;
+    }
+  });
+  return;
+}
 
 $(document).ready(function(){
 
